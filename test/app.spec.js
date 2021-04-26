aca52312af6b5b510a3a8b856cd4443b4a8b7e53
Felj Abas <felabas41@gmail.com>
2021-04-25 23:16:27 -0700

    Created migration scripts & set up migrations

--- test/app.spec.js
+++ test/app.spec.js
@@ -4,7 +4,7 @@ describe('App', () => {
     it('GET / responds with 200 containing "Hello, world!"', () => {
         return supertest(app)
             .get('/')
-            .expect(200, "Hello, world!");
+            .expect(200, "Hello, diva!");
     });
 });
 
